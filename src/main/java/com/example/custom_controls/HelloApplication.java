7679d7cd4a5745b9b9729bc9aea4f3e61ff21289
usuario <sergioleonhorea@gmail.com>
2022-12-12 13:20:42 +0100

    Parte 2 completa

--- src/main/java/com/example/custom_controls/HelloApplication.java
+++ src/main/java/com/example/custom_controls/HelloApplication.java
@@ -12,6 +12,8 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        CombinedControl combined=new CombinedControl();
+        scene.getStylesheets().add(HelloApplication.class.getResource("CSS/configuracion.css").toExternalForm());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
